c2f3463d37092076fcf4cb17291982ef88c7eec5
Sonu1507 <rsonu150794@gmail.com>
2021-08-29 16:10:34 +0530

    added search bar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import {useState,useEffect} from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
 function App() {
-  const [product,setProduct]=useState([]);
-  useEffect(()=>{
+  const [product, setProduct] = useState([]);
+  const [search, setSearch] = useState("")
+  useEffect(() => {
     axios.get('http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline')
-    .then((res)=>setProduct(res.data))
-    .catch((err)=>console.log(err))
+      .then((res) => setProduct(res.data))
+      .catch((err) => console.log(err))
 
-  },[])
-  
-    return (
+  }, [])
+  return (
 
     <div className="App">
-      {product.map((beuty)=>{
-        return(
-        <div className="beauty">
-          <img src={`${beuty.image_link}`} alt="beauty pic"/>
-          <p>{beuty.name}</p>
-          <p>price: Rs.{beuty.price}/-</p>
+      <div className="search">
+        <input type="text" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)} />
+      </div>
+      <div className="product">
+        {product.filter((p) => p.name.toLowerCase().includes(search.toLowerCase())).map((beuty) => {
+          return (
+            <div className="beauty">
+              <img src={`${beuty.image_link}`} alt="beauty pic" />
+              <p>{beuty.name}</p>
+              <p>price: Rs.{beuty.price}/-</p>
 
-          </div>
-      )})}
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
