6878f98df09410a88b8ff2ff3b917ed6e17229bc
Saritashanti <shantisarita77@gmail.com>
2021-08-29 17:25:46 +0530

    addedreload

--- src/App.js
+++ src/App.js
@@ -4,31 +4,44 @@ import "./App.css";
 
 function App() {
   const [product, setProduct] = useState([]);
-  const [search, setSearch] = useState("")
-  useEffect(() => {
-    axios.get('http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline')
-      .then((res) => setProduct(res.data))
-      .catch((err) => console.log(err))
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
 
+  useEffect(() => {
+    setLoading(true);
+    axios.get('http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline')
+      .then((res) => {
+        setProduct(res.data)
+        setLoading(false);
+      })
+      .catch((err) => console.log(err));
   }, [])
+
+
+
   return (
 
+
     <div className="App">
+      {loading ? (<div style={{ display: "flex", justifyContent: "center" }}>
+        <img src="/image/loader.gif" alt="Loading . . ." />
+      </div>) : (<>
       <div className="search">
         <input type="text" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)} />
       </div>
       <div className="product">
-        {product.filter((p) => p.name.toLowerCase().includes(search.toLowerCase())).map((beuty) => {
-          return (
-            <div className="beauty">
-              <img src={`${beuty.image_link}`} alt="beauty pic" />
-              <p>{beuty.name}</p>
-              <p>price: Rs.{beuty.price}/-</p>
+          {product.filter((p) => p.name.toLowerCase().includes(search.toLowerCase())).map((beuty) => {
+            return (
+              <div className="beauty">
+                  <img src={`${beuty.image_link}`} alt="beauty pic" />
+                  <p>{beuty.name}</p>
+                  <p>price: Rs.{beuty.price}/-</p>
 
-            </div>
-          )
-        })}
-      </div>
+              </div>
+              )
+})}
+</div></>
+      )}
     </div>
   );
 }
